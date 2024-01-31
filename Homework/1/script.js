b11f9ccac243f881760eb72b50500ca63dfb09b1
Lexuse <Lexuse2007@gmail.com>
2024-01-31 12:03:51 +0300

    Repository created

---
+++ Homework/1/script.js
@@ -0,0 +1,16 @@
+console.log("homework for the first seminar \n \n \n ")
+console.log("task 1")
+
+//task 1
+const celsiusDegrees = 26;
+let farenheitDegrees = ((9/5) * celsiusDegrees) + 32;
+
+console.log(celsiusDegrees, "Celsius degrees equivalently Farenheit degrees",  farenheitDegrees);
+
+//task 2
+console.log("\n\n")
+console.log("task 2")
+
+const myName = "Алексей";
+let admin = myName;
+console.log(admin);
---
+++ Seminars/1/script.js
@@ -0,0 +1,72 @@
+// job task 2
+console.log("task 2 \n ")
+let num1 = 123;
+console.log(num1);
+
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 1 + 2 + 3;
+console.log(b);
+
+//this is the first type of comment
+
+/*
+this id the second type of comment
+*/
+
+//job task 3
+console.log("task 3: \n ")
+let num2 = 10;
+let num3 = 2;
+
+let sum = num2 + num3;
+let diff = num2 - num3;
+let mult = num2 * num3;
+let div = num2 / num3;
+
+console.log(num2, num3 ,sum, diff, mult, div);
+
+let totalSum = sum + diff + mult + div;
+
+console.log (totalSum);
+
+//job task 4
+//23
+//6
+//25
+//13
+//4
+//4
+
+//job task 5
+console.log("task 5: \n ")
+
+let num4 = 1.5;
+let num5 = 0.75;
+let sumTask5 = num4 + num5;
+
+console.log(num4 + num5, sumTask5)
+
+let num6 = 15;
+console.log(-num6);
+
+//job task 6
+console.log("task 6: \n ")
+
+let myName = "Алексей";
+let myLastName = "Тарала";
+
+console.log(myName, myLastName);
+
+let javaString = "java";
+let scriptString = "script";
+
+console.log(javaString + scriptString);
+
+const string1 = "hello";
+const string2 = "world";
+
+console.log(string1 + " " + string2);
